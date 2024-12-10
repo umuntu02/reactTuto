b572895e51c280be1b33b66246d1d16afca863e3
ked <132898635+umuntu02@users.noreply.github.com>
2024-12-10 21:18:42 +0000

    deleting padding

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     },
   ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-6 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <div className="flex items-center justify-center">
           <div className="text-center bg-white p-5 rounded-xl w-full">
