db6b101127fab3ca07238130a20d19caa60b376d
ked <132898635+umuntu02@users.noreply.github.com>
2024-12-11 00:03:52 +0000

    adding 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 import { Loader } from "lucide-react"; // Importation du Loader de Lucide React
-
+import Link from "next/link";
 
 export default function Example() {
     return (
@@ -14,19 +14,20 @@ export default function Example() {
                     <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
                         Cette Page est en maintenance
                     </p>
-                
-                        <div className="p-8 flex items-center justify-center">
-                            {/* Loader avec animation de rotation */}
-                            <Loader className="animate-slow-spin w-12 h-12 text-mygreenblue" />
-                        </div>
-                    
+
+                    <div className="p-8 flex items-center justify-center">
+                        {/* Loader avec animation de rotation */}
+                        <Loader className="animate-slow-spin w-12 h-12 text-mygreenblue" />
+                    </div>
+
                     <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <a
-                            href="/"
-                            className="rounded-md bg-mygreenblue px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Revenir page d'accueil
-                        </a>
+                        <Link href="/" passHref>
+                            <div
+                                className="rounded-md bg-mygreenblue px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Revenir page d'accueil
+                            </div>
+                        </Link>
                     </div>
                 </div>
                 <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
