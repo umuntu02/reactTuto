826a8d1ebd8cf1b14e6ed31f53c7a6570f152dce
ked <132898635+umuntu02@users.noreply.github.com>
2024-12-11 01:51:09 +0000

    adde style remouve button bg

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,9 +25,13 @@ export default function Home() {
         },
     ];
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-15 gap-16 sm:p-12 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center">
-                <div className="flex items-center justify-center">
+
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    Welcome Devlin
+                </header>
+                <div className="flex items-center justify-center w-full">
                     <div className="text-center bg-white p-5 rounded-xl w-full">
                         <div className="flex justify-center items-center mb-4">
                             {/* Icône de dossier */}
@@ -37,12 +41,12 @@ export default function Home() {
                         </div>
                         <h2 className="text-lg font-semibold text-gray-800 mb-2">Pas de Mission</h2>
                         <p className="text-sm text-gray-500 mb-6">Commence en publiant une nouvelle mission</p>
-                        <button className="mt-4 py-2 px-6 bg-mygreenblue text-white text-sm rounded-full hover:bg-white hover:text-mygreenblue transition-all">
+                        <button className="mt-4 py-2 px-6 bg-white text-mygreenblue text-sm rounded-full hover:bg-mygreenblue hover:text-white transition-all">
                             + Nouvelle Mission
                         </button>
                     </div>
                 </div>
-                <div className="max-w-2xl mx-auto p-4">
+                <div className="max-w-2xl mx-auto p-4 w-full">
                     <div className="text-center mb-8">
                         <div className="text-1xl font-semibold text-gray-800">Invite ou cherche tes amis</div>
                         <p className="text-sm text-gray-500">
@@ -59,7 +63,7 @@ export default function Home() {
                             placeholder="Enter an email"
                             className="flex-1 px-4 py-2 border rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
-                        <button className="px-6 py-2 bg-mygreenblue text-white rounded-md hover:bg-white hover:text-mygreenblue">
+                        <button className="px-6 py-2 bg-white text-mygreenblue rounded-md hover:bg-mygreenblue hover:text-white">
                             Inviter
                         </button>
                     </div>
@@ -70,13 +74,13 @@ export default function Home() {
                             {teamMembers.map((member) => (
                                 <li key={member.name} className="flex items-center justify-between p-4 border-b">
                                     <div className="flex items-center space-x-4">
-                                    <Image
-                                        src={member.avatar} // Remplace 'member.avatar' par le bon chemin de l'image
-                                        alt={member.name}
-                                        width={48}
-                                        height={48}
-                                        className="rounded-full"  // Application du style circulaire à l'avatar
-                                        style={{ width: "auto", height: "auto", objectFit: "cover", objectPosition: "center" }}  // Maintenir le ratio d'aspect avec auto
+                                        <Image
+                                            src={member.avatar} // Remplace 'member.avatar' par le bon chemin de l'image
+                                            alt={member.name}
+                                            width={48}
+                                            height={48}
+                                            className="rounded-full"  // Application du style circulaire à l'avatar
+                                            style={{ width: "auto", height: "auto", objectFit: "cover", objectPosition: "center" }}  // Maintenir le ratio d'aspect avec auto
                                         />
 
                                         <div>
@@ -84,18 +88,18 @@ export default function Home() {
                                             <div className="text-sm text-gray-500">{member.role}</div>
                                         </div>
                                     </div>
-                                    <button className="flex items-center text-sm text-mygreenblue hover:text-blue-700">
-                                        <Plus className="w-4 h-4 mr-2" /> Inviter
+                                    <button className="px-6 py-2 flex items-center text-sm text-mygreenblue hover:text-blue-700">
+                                        Suivre
                                     </button>
                                 </li>
                             ))}
                         </ul>
                     </div>
                 </div>
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <div className="text-sm font-small text-gray-800">Powered by devlin_corp</div>
-            </footer>
-        </div>
+
+            </div>
+
+        </section>
+
     );
 }
